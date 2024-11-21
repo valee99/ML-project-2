bccf0a4a3d7be7f286addac425706d21dd3d8f52
Martin Le Bras <martin.lebras0@gmail.com>
2024-11-21 01:46:57 +0100

    ml/Adding load_data and create_split scripts from notebooks

---
+++ src/create_dataset.py
@@ -0,0 +1,113 @@
+import os
+import random
+import shutil
+import argparse
+from glob import glob
+from os.path import basename
+import random
+
+
+def split_files(path_labeled_dir: str, splits: dict, all_slices: bool) -> dict:
+
+    images_path = glob(os.path.join(path_labeled_dir, "*.jpg"))
+    labels_path = glob(os.path.join(path_labeled_dir, "*.txt"))
+
+    files_with_labels = [basename(path).split(".")[0] for path in images_path]
+    files_without_labels = [
+        basename(path).split(".")[0]
+        for path in labels_path
+        if basename(path).split(".")[0] not in files_with_labels
+    ]
+
+    random.shuffle(files_with_labels)
+    random.shuffle(files_without_labels)
+
+    if all_slices:
+        files = [files_with_labels, files_without_labels]
+    else:
+        files = [files_with_labels]
+
+    train_files = []
+    val_files = []
+    test_files = []
+
+    for file_list in files:
+
+        n_files = len(file_list)
+        train_files += file_list[: int(n_files * splits["train"])]
+        val_files += file_list[
+            int(n_files * splits["train"]) : int(n_files * splits["train"])
+            + int(n_files * splits["val"])
+        ]
+        test_files += file_list[
+            int(n_files * splits["train"]) + int(n_files * splits["val"]) :
+        ]
+
+    return {"train": train_files, "val": val_files, "test": test_files}
+
+
+def move_files(files: dict, path_labeled_dir: str, path_split_dir: str):
+
+    for split, files_list in files.items():
+
+        for file_name in files_list:
+
+            image_file = os.path.join(path_labeled_dir, file_name + ".jpg")
+            split_image_file = os.path.join(
+                path_split_dir, split, "images", file_name + ".jpg"
+            )
+            shutil.copy(image_file, split_image_file)
+
+            label_file = os.path.join(path_labeled_dir, file_name + ".txt")
+            split_label_file = os.path.join(
+                path_split_dir, split, "labels", file_name + ".txt"
+            )
+
+            if os.path.exists(label_file):
+                shutil.copy(label_file, split_label_file)
+
+
+def main(
+    path_labeled_dir: str,
+    path_split_dir: str,
+    train_ratio: float,
+    val_ratio: float,
+    test_ratio: float,
+    all_slices: bool,
+):
+
+    dataset_path = os.path.join(path_split_dir, basename(path_labeled_dir))
+    os.makedirs(dataset_path, exist_ok=True)
+
+    splits = {"train": train_ratio, "val": val_ratio, "test": test_ratio}
+    for split, ratio in splits.items():
+        if ratio > 0:
+            os.makedirs(os.path.join(dataset_path, split, "images"), exist_ok=True)
+            os.makedirs(os.path.join(dataset_path, split, "labels"), exist_ok=True)
+
+    files = split_files(path_labeled_dir, splits, all_slices)
+
+    move_files(files, path_labeled_dir, dataset_path)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A script for processing files.")
+    parser.add_argument(
+        "--path_labeled", type=str, default="./data/data_labeled/ctrst-210-255"
+    )
+    parser.add_argument("--path_split", type=str, default="./data/data_split")
+    parser.add_argument("--train_ratio", type=float, default=0.7)
+    parser.add_argument("--val_ratio", type=float, default=0.2)
+    parser.add_argument("--test_ratio", type=float, default=0.1)
+    parser.add_argument("--all_slices", type=bool, default=True)
+    args = parser.parse_args()
+
+    main(
+        args.path_labeled,
+        args.path_split,
+        args.train_ratio,
+        args.val_ratio,
+        args.test_ratio,
+        args.all_slices,
+    )
---
+++ src/load_from_raw.py
@@ -0,0 +1,178 @@
+import os
+import json
+import numpy as np
+import tifffile as tiff
+from PIL import Image
+import argparse
+
+
+def get_geojson_files(geojson_dir: str) -> set:
+    """
+    Get set of geoJSON file names without extensions.
+    """
+    return {
+        os.path.splitext(f)[0]
+        for f in os.listdir(geojson_dir)
+        if f.endswith(".geojson")
+    }
+
+
+def process_slice(
+    slice_idx: int,
+    geojson_data: dict,
+    class_mapping: dict,
+    img_height: int,
+    img_width: int,
+) -> str:
+
+    slice_features = [
+        feature
+        for feature in geojson_data["features"]
+        if feature["geometry"]["plane"]["t"] == slice_idx
+    ]
+
+    if slice_features != []:
+
+        slice_labels = []
+        for feature in slice_features:
+            classification = feature["properties"]["classification"]["name"]
+            class_id = class_mapping.get(classification, class_mapping["Non-Living"])
+            coordinates = feature["geometry"]["coordinates"][0]
+
+            x_coords = [point[0] for point in coordinates]
+            y_coords = [point[1] for point in coordinates]
+            min_x, max_x = int(min(x_coords)), int(max(x_coords))
+            min_y, max_y = int(min(y_coords)), int(max(y_coords))
+
+            x_center = (min_x + max_x) / 2 / img_width
+            y_center = (min_y + max_y) / 2 / img_height
+            width = (max_x - min_x) / img_width
+            height = (max_y - min_y) / img_height
+            slice_labels.append(f"{class_id} {x_center} {y_center} {width} {height}")
+        slice_labels = "\n".join(slice_labels)
+
+    else:
+
+        slice_labels = ""
+
+    return slice_labels
+
+
+def adjust_contrast(
+    slice_array: np.ndarray, min_range: int, max_range: int
+) -> np.ndarray:
+
+    slice_min, slice_max = np.min(slice_array), np.max(slice_array)
+    normalized_slice = (slice_array - slice_min) / (slice_max - slice_min)
+    adjusted_slice_array = min_range + normalized_slice * (max_range - min_range)
+    return adjusted_slice_array
+
+
+def process_geojson_file(
+    file_name: str,
+    tif_dir: str,
+    geojson_dir: str,
+    output_label_dir: str,
+    class_mapping: dict,
+    min_range: int,
+    max_range: int,
+    img_height: int,
+    img_width: int,
+):
+    """
+    Process a single geoJSON file and its corresponding .tif file.
+    """
+    tif_path = os.path.join(tif_dir, file_name + ".tif")
+    geojson_path = os.path.join(geojson_dir, file_name + ".geojson")
+
+    # Check if the corresponding .tif file exists
+    if not os.path.exists(tif_path):
+        raise (f"Error: `{tif_path}` file not found.")
+
+    # Load geoJSON data
+    with open(geojson_path, "r") as file:
+        geojson_data = json.load(file)
+
+    # Open the .tif file and process each frame specified in the geoJSON
+    img_stack = tiff.imread(tif_path)
+    for slice_idx, slice_array in enumerate(img_stack):
+        slice_labels = process_slice(
+            slice_idx, geojson_data, class_mapping, img_height, img_width
+        )
+        adjust_slice_array = adjust_contrast(slice_array, min_range, max_range)
+        adjusted_image = Image.fromarray(adjust_slice_array.astype(np.uint8))
+
+        image_path = os.path.join(
+            output_label_dir,
+            f"{file_name}_ctrst-{min_range}-{max_range}_slice-{slice_idx}.jpg",
+        )
+        label_path = os.path.join(
+            output_label_dir,
+            f"{file_name}_ctrst-{min_range}-{max_range}_slice-{slice_idx}.txt",
+        )
+
+        adjusted_image.save(image_path)
+        if slice_labels != "":
+            with open(label_path, "w") as label_file:
+                label_file.write(slice_labels)
+
+
+def main(
+    tif_dir: str,
+    geojson_dir: str,
+    output_label_dir: str,
+    class_mapping: dict,
+    min_range: int,
+    max_range: int,
+    img_height: int,
+    img_width: int,
+):
+
+    os.makedirs(output_label_dir, exist_ok=True)
+    geojson_files = get_geojson_files(geojson_dir)
+    for file_name in geojson_files:
+        process_geojson_file(
+            file_name,
+            tif_dir,
+            geojson_dir,
+            output_label_dir,
+            class_mapping,
+            min_range,
+            max_range,
+            img_height,
+            img_width,
+        )
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A script for processing files.")
+    parser.add_argument(
+        "--path_geojson", type=str, default="./data/data_raw/geojson_file"
+    )
+    parser.add_argument("--path_images", type=str, default="./data/data_raw/images")
+    parser.add_argument("--path_output", type=str, default="./data/data_labeled")
+    parser.add_argument("--min_contrast", type=int, default=210)
+    parser.add_argument("--max_contrast", type=int, default=255)
+    args = parser.parse_args()
+
+    # Image dimensions for YOLO normalization
+    IMAGE_WIDTH = 1616
+    IMAGE_HEIGHT = 1240
+
+    CLASS_MAPPING = {"Living": 0, "Non-Living": 1, "Bubble": 2}
+
+    PATH_OUTPUT_LABELED_DIR = (
+        f"{args.path_output}/ctrst-{args.min_contrast}-{args.max_contrast}"
+    )
+
+    main(
+        args.path_images,
+        args.path_geojson,
+        PATH_OUTPUT_LABELED_DIR,
+        CLASS_MAPPING,
+        args.min_contrast,
+        args.max_contrast,
+        IMAGE_HEIGHT,
+        IMAGE_WIDTH,
+    )
